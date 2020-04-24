3f6f231d2f4e60f9afd9eaa9f6fb555b9ba0dc05
sikren <57663671+sikren@users.noreply.github.com>
2020-04-24 15:43:20 +0300

    web's base
    Login page (without database adding), Data (init + user model), some http templates

---
+++ main.py
@@ -0,0 +1,25 @@
+from flask import Flask, render_template
+from data import db_session
+from forms.LoginForm import LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return 'success'
+    return render_template('LoginTemplate.html', form=form,
+                           title='Login')
+
+
+@app.route('/register')
+def register():
+    pass
+
+
+if __name__ == '__main__':
+    db_session.global_init("db/data.sqlite")
+    app.run(port=8080, host='127.0.0.1')
